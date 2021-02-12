0fd4c2a6486e2cd8c6cbd35b3a8d3fa10da543e9
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-02-13 10:31:07 +1100

    fixing inquirer to work

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ let employees = [];
 //prompt user input
 const teamMember = () => {
  inquirer.prompt([
-            {
+// function teamMember(){
+//     const questions = [
+           {
                 name: "name",
                 type: "input",
                 message: "What is your name?",
@@ -26,14 +28,7 @@ const teamMember = () => {
             {
                 name: "id",
                 type: "input",
-                message: "What is your employee id?",
-                // validate: function (answer) {
-                //     if (answer.length < 1) {
-                //         return console.log("A valid email is required.");
-                //     }
-                //     return true;
-                // }
-     
+                message: "What is your employee id?",     
             },
             {
                 name: "email",
@@ -50,7 +45,7 @@ const teamMember = () => {
                 name: 'role',
                 type: 'list',
                 message: 'What is your role?',
-                choices: ['Manager', 'Engineer', 'Intern']
+                choices: ['manager', 'engineer', 'intern']
             },
      
         {
@@ -87,27 +82,42 @@ const teamMember = () => {
     .then((answer) => {
         console.log(answer.name)
     })
-}; teamMember()
-// teamMember()
-// function buildTeamList() {
-//     inquire.prompt(teamMember).then(employeeInfo => {
-//         if (employeeInfo.role == "Engineer") {
-//             var newMember = new Engineer(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.github);
-//         }else if (employeeInfo.role == "Manager") {
-//             var newMember = new Manager(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.officeNumber);
-//         }  
-//         else {
-//             var newMember = new Intern(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.school);
-//         }
-//         teamList.push(newMember);
-//         if (employeeInfo.addAnother === "Yes") {
-//             console.log(" ");
-//             buildTeamList();
-//         } else {
-//             buildHtmlPage();
-//         }
-//     })
-// }
+    }; teamMember()
+
+function buildTeamList(teamMember) {
+    switch(list.role){
+        case 'manager':
+            console.log('this person is manager')
+            break;
+        case 'engineer':
+           console.log('engineer')
+           break;
+        case('intern'):
+            console.log('intern')
+            break;
+        default:
+            console.log('please chose your role')
+
+        // inquirer.prompt(teamMember).then(answer => {
+        //     if (answer.role == "Engineer") {
+        //         var newMember = new Engineer(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.github);
+        //     }else if (answer.role == "Manager") {
+        //         var newMember = new Manager(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.officeNumber);
+        //     }  
+        //     else {
+        //         var newMember = new Intern(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.school);
+        //     }
+        //     teamList.push(newMember);
+        //     if (employeeInfo.addAnother === "Yes") {
+        //         console.log(" ");
+        //         buildTeamList();
+        //     } else {
+        //         buildHtmlPage();
+        //     }
+        // })
+    
+    }
+}; buildTeamList()
 //    buildTeamList()         
 
 //generate a html based on the user input
