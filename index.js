1cdf349ea3aca3c6236a75b66db77568fecdfa16
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-02-12 16:42:19 +1100

    commit to get inqurer work

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-const Engineer = require('./lib/Engineer');
-const fs = requirer('fs');
-const path = reqquire('path');
+const fs = require('fs');
+const path = require('path');
 
 //internal files
 const Employee = require('./lib/Employee.js');
@@ -12,8 +11,7 @@ const Intern = require('./lib/Intern');
 let employees = [];
 //prompt user input
 function teamMember() {
-    // Ask questions to gather information about manager. Save to an manager object.
-    
+    // Ask questions to gather information about employees.
 
         inquirer.prompt([
             {
@@ -39,55 +37,81 @@ function teamMember() {
                 name: 'role',
                 message: 'What is your role?',
                 choices: ['Manager', 'Engineer', 'Intern']
+            },
+     
+        {
+            when: list => {
+                return list.role == "Manager"
+            },
+            type: "input",
+            name: "officeNumber",
+            message: "Enter your office number",
+            validate: async (input) => {
+                if (input == "" || /\s/.test(input)) {
+                    return "Please enter a valid office number";
+                }
+                return true;
             }
-        ])
-        .then(function (answers) {
-            let employee = new Employee(answers.name, answers.id, answers.email, answers.role);
-            engagementTeam.push(Employee)
-            chooseMemberNext()
-        })
-        .catch(function(err) {
-            console.log(err);
-          });
-
-        if ( employee.amswer.role === 'Engineer') {
-
-            inquirer.prompt([
-            {
-                    type: "input",
-                    message: "What is your your GitHub username?",
-                    name: "github",
-                    validate: confirmName
+        },
+        {
+            when: list => {
+                return list.role == "Engineer"
+            },
+            type: "input",
+            name: "github",
+            message: "Engineer, enter your github username:",
+            validate: async (input) => {
+                if (input == "" || /\s/.test(input)) {
+                    return "Please enter a valid GitHub username";
                 }
-            ])
-
-                .then(function (answers) {
-                    let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-                    engagementTeam.push(engineer);
-                    chooseMemberNext();
-                })
-                .catch(function(err) {
-                    console.log(err);
-                  });
+                return true;
+            }
+        },
+        {
+            when: list => {
+                return list.role == "Intern"
+            },
+            type: "input",
+            name: "school",
+            message: "Enter the school name you graduated",
+            validate: async (input) => {
+                if (input == "" || /\s/.test(input)) {
+                    return "Please enter a valid school name";
                 }
+                return true;
+            }
+        },
+        {
+            type: "list",
+            name: "addAnother",
+            message: "Add another team member?",
+            choices: ["Yes", "No"]
+        }
+    ]);
+}
+function buildTeamList() {
+    inquire.prompt(teamMember).then(employeeInfo => {
+        if (employeeInfo.role == "Engineer") {
+            var newMember = new Engineer(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.github);
+        }else if (employeeInfo.role == "Manager") {
+            var newMember = new Manager(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.officeNumber);
+        }  
+        else {
+            var newMember = new Intern(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.school);
+        }
+        teamList.push(newMember);
+        if (employeeInfo.addAnother === "Yes") {
+            console.log(" ");
+            buildTeamList();
+        } else {
+            buildHtmlPage();
+        }
+    })
+}
             
 
- 
 
-//adding more team member
-          async function chooseMemberNext() {
-            try {
-
-                let teamChoice = await inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'team',
-                        message: 'Which type of team member would you like to add',
-                        choices: ['Manager', 'Engineer', 'Intern', 'I don/t want to add anymore team members.']
-                    }
-                ]);
-            
-          // Depending on the response, loop through questions to gather information and save to appropriate object
+ // Depending on the response, loop through questions to gather information and save to appropriate object
           
 
 //generate a html based on the user input
