231bbea09ab9d9ab85e47b7a55eb246e3dea5a5c
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-02-13 16:16:16 +1100

    inquirer for each role fixed

--- index.js
+++ index.js
@@ -8,11 +8,44 @@ const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
-let employees = [];
+//creating arrays
+const manager = [];
+const engineer = [];
+const intern = [];
 
- 
+
+
+// 1 create one inquirer prompt to ask which role
+// 2 .then navigate to each function depend on which role they choose
+// 3. depend of the role, having different question and after they answered it,
+
+const chooseRole = () => {
+    inquirer.prompt([
+    {
+        name: 'role',
+        type: 'list',
+        message: 'What is your role?',
+        choices: ['manager', 'engineer', 'intern']
+        }
+    ])
+.then ((data) => {
+    console.log(data.role)
+switch(data.role){
+    case 'manager':
+        runManagerQuestions()
+        break;
+    case 'engineer':
+        runEngineerQuestions()
+        break;
+    case 'intern':
+        runInternQuestions()
+        break;
+}
+    });
+};
+chooseRole();
 //prompt user input
-const teamMember = () => {
+const runManagerQuestions = () => {
  inquirer.prompt([
            {
                 name: "name",
@@ -41,76 +74,123 @@ const teamMember = () => {
                     return true;
                 }
             },
-            {
-                name: 'role',
-                type: 'list',
-                message: 'What is your role?',
-                choices: ['manager', 'engineer', 'intern']
+            {                 
+                name: "officeNumber",
+                type: "input",
+                message: "Enter your office number",
             },
-        {
+            {
             name: "addAnother",
             type: "list",
             message: "Add another team member?",
-            choices: ["Yes", "No"]
+            choices: ["Yes", "No"],
         },
+
     ])
-    .then(answers => {
-        if(answers.role === 'manager'){
-            runManagerQuestions()
-        }
-    })
-    .then(data => {
-        if(data.role === 'engineer'){
-            runEngineerQuestions()
-        }
-    })
-    .then(data => {
-        if(data.role === 'Intern'){
-            runInternQuestions()
-        }
-    });
-
-
-
- const runManagerQuestions = () => {
-    inquirer.prompt([ 
-    {                 
-        name: "officeNumber",
-        type: "input",
-        message: "Enter your office number",
+    .then ((data) => {
+        console.log(data.addAnother)
+    switch(data.addAnother){
+        case 'Yes':
+            chooseRole()
+            break;
+        case 'No':
+            console.log('no more team member')
+            break;
     }
-    ])
-    .then((answer) => {
-        buildTeamList()
-        getMainHtml()
-    })
-    } 
-    };
-
- const runEngineerQuestions = () => {
-        inquirer.prompt([ 
-        {                 
-            name: "github",
-            type: "input",
-            message: "Engineer, enter your github username:",
-            validate: function (answer) {
-                if (answer.length < 1) {
-                    return console.log("A valid email is required.");
-                }
-                return true;
-            }
-        }
-        ])
-        .then((answer) => {
-            buildTeamList()
-            getMainHtml()
         })
-        }; 
+};
+ const runEngineerQuestions = () => {
+        inquirer.prompt([
+                  {
+                       name: "name",
+                       type: "input",
+                       message: "What is your name?",
+                       validate: function (answer) {
+                           if (answer.length < 1) {
+                               return console.log("A valid name is required.");
+                           }
+                           return true;
+                       }
+                   },
+                   {
+                       name: "id",
+                       type: "input",
+                       message: "What is your employee id?",     
+                   },
+                   {
+                       name: "email",
+                       type: "input",
+                       message: "What is your email?",
+                       validate: function (answer) {
+                           if (answer.length < 1) {
+                               return console.log("A valid email is required.");
+                           }
+                           return true;
+                       }
+                   },
+                   {                 
+                    name: "github",
+                    type: "input",
+                    message: "Engineer, enter your github username:",
+                    validate: function (answer) {
+                        if (answer.length < 1) {
+                            return console.log("A valid email is required.");
+                        }
+                        return true;
+                    }
+                   },
+                   {
+                    name: "addAnother",
+                    type: "list",
+                    message: "Add another team member?",
+                    choices: ["Yes", "No"],
+                },
         
-const runInternrQuestions = () => {
-            inquirer.prompt([ 
-            {                 
-                name: "school",
+            ])
+            .then ((data) => {
+                console.log(data.addAnother)
+            switch(data.addAnother){
+                case 'Yes':
+                    chooseRole()
+                    break;
+                case 'No':
+                    console.log('no more team member')
+                    break;
+            }
+                })
+            
+};
+ const runInternQuestions = () => {
+            inquirer.prompt([
+                      {
+                           name: "name",
+                           type: "input",
+                           message: "What is your name?",
+                           validate: function (answer) {
+                               if (answer.length < 1) {
+                                   return console.log("A valid name is required.");
+                               }
+                               return true;
+                           }
+                       },
+                       {
+                           name: "id",
+                           type: "input",
+                           message: "What is your employee id?",     
+                       },
+                       {
+                           name: "email",
+                           type: "input",
+                           message: "What is your email?",
+                           validate: function (answer) {
+                               if (answer.length < 1) {
+                                   return console.log("A valid email is required.");
+                               }
+                               return true;
+                           }
+                       },
+                       {                 
+                        name: "school",
                 type: "input",
                 message: "Enter the school name you graduated",
                 validate: function (answer) {
@@ -119,53 +199,69 @@ const runInternrQuestions = () => {
                     }
                     return true;
                 }
-            }
-            ])
-            .then((answer) => {
-                buildTeamList()
-                getMainHtml()
-            })
-            }; 
+                       },
+                
+                       {
+                        name: "addAnother",
+                        type: "list",
+                        message: "Add another team member?",
+                        choices: ["Yes", "No"],
+                    },
             
-  
-    teamMember()
+                ])
+                .then ((data) => {
+                    console.log(data.addAnother)
+                switch(data.addAnother){
+                    case 'Yes':
+                        chooseRole()
+                        break;
+                    case 'No':
+                        console.log('no more team member')
+                        break;
+                }
+                    })
+};
+// add the answer data to an array or string
+// 4. after adding the data, it comes back to first function
+// 5. if user chooses to stop adding employees, then call a function that uses 
+//all the data from the various prompts and use it to write a file
 
-    function buildTeamList(teamMember) {
-        switch(list.role){
-            case 'manager':
-                var newMember = new Manager(teamMember.name, employees.length + 1, teamMember.id, teamMember.email, teamMember.officeNumber)
-                break;
-            case 'engineer':
-                var newMember = new Engineer(teamMember.name, employees.length + 1, teamMember.id, teamMember.email, teamMember.github)
-                break;
-            case('intern'):
-                var newMember = new Manager(teamMember.name, employees.length + 1, teamMember.id, teamMember.email, teamMember.school)
-                break;
-            default:
-                console.log('please chose your role');
+    // function buildTeamList(teamMember) {
+    //     switch(answer.role){
+    //         case 'manager':
+    //             var newMember = new Manager(teamMember.name, employees.length + 1, teamMember.id, teamMember.email, teamMember.officeNumber)
+    //             break;
+    //         case 'engineer':
+    //             var newMember = new Engineer(teamMember.name, employees.length + 1, teamMember.id, teamMember.email, teamMember.github)
+    //             break;
+    //         case('intern'):
+    //             var newMember = new Intern(teamMember.name, employees.length + 1, teamMember.id, teamMember.email, teamMember.school)
+    //             break;
+    //         default:
+    //             console.log('please chose your role');
     
-             employees.push(newMember)
-        }
-        switch(list.addAnother){
-            case 'Yes':
-                teamMember()
-                break;
-            case 'No':
-                getMainHtml()
-                break;
+    //          employees.push(newMember)
+    //     }
+    //     switch(list.addAnother){
+    //         case 'Yes':
+    //             teamMember()
+    //             break;
+    //         case 'No':
+    //             getMainHtml()
+    //             break;
 
-        }
+    //     }
          
-    };
-    //   buildTeamList()      
+    // };
+    
 
 //generate a html based on the user input
-function getMainHtml() {
-    const htmlPath = path.join(__dirname, "src", "html", "main.html");
+// function getMainHtml() {
+//     const htmlPath = path.join(__dirname, "src", "html", "main.html");
 
-    const html = fs.readFileSync(htmlPath, "utf-8");
-    return html;
-}
+//     const html = fs.readFileSync(htmlPath, "utf-8");
+//     return html;
+// }
 
 
 // console.log(getMainHtml());
@@ -176,22 +272,22 @@ function getMainHtml() {
 //     "{{ cards }}",
 //     employees.role
 // );
-for (let index = 0; index < employees.length; index++) {
-    const employee = employees[index];
+// for (let index = 0; index < employees.length; index++) {
+//     const employee = employees[index];
 
 
-    let result = htmlParser.replaceTemplate(
-        mainHtml,
-        "{{ name }}",
-        employees.name
-    );
+//     let result = htmlParser.replaceTemplate(
+//         mainHtml,
+//         "{{ name }}",
+//         employees.name
+//     );
 
-    result = htmlParser.replaceTemplate(mainHtml, "{{ email }}", employees.email);
-    result = htmlParser.replaceTemplate(mainHtml, "{{ id }}", employees.id);
+//     result = htmlParser.replaceTemplate(mainHtml, "{{ email }}", employees.email);
+//     result = htmlParser.replaceTemplate(mainHtml, "{{ id }}", employees.id);
 
 
-console.log(result);
-const outputPath = path.join(__dirname, "dist", "output.html");
+// console.log(result);
+// const outputPath = path.join(__dirname, "dist", "output.html");
 
-fs.writeFileSync(outputPath, result);
-    };
+// fs.writeFileSync(outputPath, result);
+//     };
